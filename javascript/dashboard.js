c82de7dfbff07915cc1e4750fc76b56fd2a09c62
misthi <misthirawal@gmail.com>
2026-05-13 07:46:56 +0530

    dashboard loads sidebar and navbar using asynchronousfunction

---
+++ javascript/dashboard.js
@@ -0,0 +1,14 @@
+async function navbar_load()//async bcz this func may take time to load in browser
+{
+    const response= await fetch("./navbar.html"); //fetch from relative path my reusable block of navigation code
+    const navigation = await response.text();//response is not html it contains response object so text() convert the content into text to read
+    document.getElementById("navigation_container").innerHTML=navigation
+}
+navbar_load();
+async function sidebar_load()
+{
+    const reply=await fetch("./Sidebar.html");
+    const sidebar= await reply.text();
+    document.getElementById("sidebar_container").innerHTML=sidebar;
+}
+sidebar_load();
